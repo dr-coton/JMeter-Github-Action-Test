1b31822f6d425a67c0be8b186dee87f011a4d97b
dr.coton <54978448+dr-coton@users.noreply.github.com>
2023-03-30 00:18:31 +0900

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+
+
+#!/bin/sh
+echo "Execution is being started"
+echo "**************************"
+jmeter $@
+echo "**************************"
+echo "Execution has been completed, please check the artifacts to download the results."
